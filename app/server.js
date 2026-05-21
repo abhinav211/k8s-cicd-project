05d1f3e9b8c601c09a7719441a2b1376eb613867
abhinav211 <abhivatssan@gmail.com>
2026-05-21 17:17:59 +0530

    Full Kubernetes CI/CD project

---
+++ app/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const ENVIRONMENT = process.env.ENVIRONMENT || "dev";
+
+const employees = [
+  { id: 1, name: "John" },
+  { id: 2, name: "David" },
+  { id: 3, name: "Sarah" }
+];
+
+app.get("/", (req, res) => {
+  res.send(`
+    <<h1>Employee App Version 2</h1>
+    <h2>Environment: ${ENVIRONMENT}</h2>
+    <pre>${JSON.stringify(employees, null, 2)}</pre>
+  `);
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "UP"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
